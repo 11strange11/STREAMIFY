da046c7a701c996a8c87e0335af5b6589b32d86e
11Strange11 <anuragpipil3@gmail.com>
2025-09-05 03:27:43 +0530

    Signup endpoint in the backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,73 @@
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
 export async function signup(req, res) {
-  res.send("Signup Route");
+  const { fullName, email, password } = req.body;
+
+  try {
+    // Validate required fields
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // Validate password length
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    // Validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Email already exists, please use a different one" });
+    }
+
+    // Generate random avatar
+    const idx = Math.floor(Math.random() * 100) + 1;
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+    // Create new user
+    const newUser = new User.create({
+      fullName,
+      email,
+      password,
+      profilePic: randomAvatar,
+    });
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: newUser._id },
+      process.env.JWT_SECRET_KEY,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    // Set cookie
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(201).json({ success: true, user: newUser });
+
+    return res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    console.error("Error in signup controller:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 export async function login(req, res) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import "dotenv/config";
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
