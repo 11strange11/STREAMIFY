5b3852efbc43f03f78d762032276a8b6f4c8ba79
11Strange11 <anuragpipil3@gmail.com>
2025-09-05 19:05:47 +0530

    Added accept friend request Endpoint in the backend

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -66,23 +66,53 @@ export async function sendFriendRequest(req, res) {
       ],
     });
 
-
     if (existingRequest) {
-      return res
-        .status(400)
-        .json({ message: "A friend request already exists between you and this user" });
+      return res.status(400).json({
+        message: "A friend request already exists between you and this user",
+      });
     }
-    
+
     const friendRequest = await FriendRequest.create({
-        sender: myId,
-        recipient: recipientId,
+      sender: myId,
+      recipient: recipientId,
     });
 
     res.status(201).json(friendRequest);
-
-
   } catch (error) {
     console.error("Error in sendFriendRequest controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function acceptFriendRequest(req, res) {
+  try {
+    const { id: requestId } = req.params;
+    const friendRequest = await FriendRequest.findById(requestId);
+
+    if (!friendRequest) {
+      return res.status(404).json({ message: "Friend request not found" });
+    }
+    //Verify if the current user is the recipient of the friend request
+    if (friendRequest.recipient.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to accept this request" });
+    }
+
+    friendRequest.status = "accepted";
+    await friendRequest.save();
+
+    // Adding both users to each other's friends list
+    await User.findByIdAndUpdate(friendRequest.sender, {
+      $addToSet: { friends: friendRequest.recipient },
+    });
+    await User.findByIdAndUpdate(friendRequest.recipient, {
+      $addToSet: { friends: friendRequest.sender },
+    });
+
+    res.status(200).json({ message: "Friend request accepted" });
+  } catch (error) {
+    console.log("Error in acceptFriendRequest controller", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,18 +1,21 @@
 import express from "express";
-import { getRecommendedUsers, getMyFriends, sendFriendRequest } from "../controllers/user.controller.js";
+import {
+  getRecommendedUsers,
+  getMyFriends,
+  sendFriendRequest,
+  acceptFriendRequest,
+} from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
-
-
 const router = express.Router();
-
 
 // Applies auth middleware to all routes
 router.use(protectRoute);
 
 router.get("/", getRecommendedUsers);
-router.get("/friends" , getMyFriends);
+router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
 export default router;
