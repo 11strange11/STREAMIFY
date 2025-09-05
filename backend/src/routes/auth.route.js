118346dbaa16309eac50e3f2388bf61ec46a2c9d
11Strange11 <anuragpipil3@gmail.com>
2025-09-05 14:45:32 +0530

    Onboarding endpoint in the endpoint

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { onboard } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", logout); // it is post because post method is for operations that change the server state
+
+router.post("/onboarding", protectRoute ,onboard);
+
+//checks if user is logged in or not
+router.get("/me", protectRoute, (req,res) => {
+    res.status(200).json({success: true, user: req.user})
+});
 
 export default router;
